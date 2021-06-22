c71d79a218d53391c67b0befd1a800cd96f0e799
Asma Maryyum <asmamaryyum@gmail.com>
2021-06-22 20:50:43 +0500

    Fixes search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -131,7 +131,7 @@ export default function Header({ onCartClick }: Props) {
                 <Dialog
                     as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={() => setSearchState(false)}
                 >
-                    <div className="min-h-screen px-4 text-center">
+                    <div className="text-center">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -155,10 +155,10 @@ export default function Header({ onCartClick }: Props) {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-sm">
+                            <div className="inline-block w-full max-w-md overflow-hidden text-left align-top m-20 transition-all transform bg-white shadow-xl rounded-full">
                                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900"></Dialog.Title>
 
-                                <Search/>
+                                <Search onCloseClicked={() => setSearchState(false)}/>
                             </div>
                         </Transition.Child>
                     </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { XIcon } from '@heroicons/react/outline';
 
+interface Props {
+    onCloseClicked: () => void;
+}
 
-export default function Search {
 
-    <div>
-        <button className="hover:bg-gray-50" onClick={() => (false)}>
-            <XIcon className="h-4 w-4"/>
+export default function Search(props: Props) {
+return (
+    <div className="flex items-center">
+        <input type="text" name="name" placeholder="Search here" className="rounded-full py-2 px-6 w-full focus:outline-none" />
+        <button className="px-4 focus:outline-none" onClick={props.onCloseClicked}>
+            <XIcon className="h-5 w-5 text-gray-400 hover:text-gray-900"/>
         </button>
-        <input type="text" name="name" placeholder="Search here" className="border border-gray-200 rounded py-2 px-3 focus:outline-none" />
     </div>
+    );
 }
